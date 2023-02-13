24c37e90221e9ebc0cf1c7122bd4af26d81c0b45
Esad Abdullah Kösedağ <81075846+kaesit@users.noreply.github.com>
2023-02-13 12:05:44 +0300

    Update image.py

--- image.py
+++ image.py
@@ -2,10 +2,10 @@ import cv2
 
 img = cv2.imread('images/uav_in_air5.jpg')
 
-with open('model2/classes.txt', 'r') as f:
+with open('model/classes.txt', 'r') as f:
     classes = f.read().splitlines()
 
-net = cv2.dnn.readNet('model2/yolov4-obj.cfg', 'model2/yolov4-obj_last.weights')
+net = cv2.dnn.readNet('model/yolov4-obj.cfg', 'model/yolov4-obj_last.weights')
 
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(scale=1 / 255, size=(410, 410), swapRB=True)
@@ -19,13 +19,13 @@ for (classId, score, box) in zip(classIds, scores, boxes):
 
     text2 = (classes[classId[0]].capitalize() + " " + str(int(score[0] * 100)) + "%")
     if (score[0] * 100 >= 99.5):      
-        print("Doğruluk oranı%: 100")
+        print("Accuracy rate %: 100")
         text2 = (classes[classId[0]].capitalize() + " 100%")
     elif (score[0] * 100 == 84.5):
-        print("Doğruluk oranı%:85")
+        print("Accuracy rate %:85")
         text2 = (classes[classId[0]].capitalize() + " 85%")
     else:
-        print("Doğruluk oranı %:", score[0] * 100)
+        print("Accuracy rate %:", score[0] * 100)
 
     text = '%s: %.2f' % (classes[classId[0]], score)
     cv2.putText(img, text2, (box[0], box[1] - 2), cv2.FONT_HERSHEY_SIMPLEX, 1,
